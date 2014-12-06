45ffd6cbdbe32de0e6f770faa5f81efc4782d262
sfurlani <stephen.furlani@gmail.com>
2014-12-06 16:44:46 -0500

    This doesn't really work

--- Root/AppDelegate.m
+++ Root/AppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "AppDelegate.h"
+#import "UIStoryboard+Tabbed.h"
+#import "UITabBarController+Storyboard.h"
 
 @interface AppDelegate ()
 
@@ -17,8 +19,18 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSArray *tabs = @[[UIStoryboard firstStoryboard],
+                      [UIStoryboard secondStoryboard]];
+    
+    UITabBarController * tabBarController = [UITabBarController tabBarControllerWithStoryboardTabs:tabs];
+    self.window = [UIWindow new];
+    [self.window setScreen:[UIScreen mainScreen]];
+    [self.window setRootViewController:tabBarController];
+    [self.window makeKeyAndVisible];
+
     return YES;
 }
+
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
---
+++ Root/UITabBarController+Storyboard.h
@@ -0,0 +1,15 @@
+//
+//  UITabBarController+Storyboard.h
+//  Root
+//
+//  Created by Mobile Dev on 12/6/14.
+//  Copyright (c) 2014 Cleod9. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITabBarController (Storyboard)
+
++ (UITabBarController *)tabBarControllerWithStoryboardTabs:(NSArray *)tabs;
+
+@end
